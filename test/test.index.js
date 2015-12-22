978eed39d7066a00958d5dd32ec742162b0ebc74
Thami Bouchnafa <thami.bouchnafa@1und1.de>
2015-12-22 14:24:08 +0100

    module.exports webserver

--- test/test.index.js
+++ test/test.index.js
@@ -1,12 +1,12 @@
 "use strict";
 var assert = require('assert');
+var pjson = require('../package.json');
 
 describe('Webserver Class', function () {
 
     var websites;
-    var Webserver = require('../webserver.js');
-    var server = new Webserver('./resources/websites');
-
+    var Webserver = require('../index.js');
+    var server = new Webserver('./resources/websites', pjson.config.port);
     before(function (done) {
         server.start(function () {
             websites = server.getAvailableWebsites();
@@ -21,6 +21,8 @@ describe('Webserver Class', function () {
     describe('#start()', function () {
         it('check if websites were found', function (done) {
             this.timeout(60000);
+
+            assert.equal(server.port, pjson.config.port);
 
             var count = 0;
             for (var property in websites) {
