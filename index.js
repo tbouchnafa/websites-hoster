28c8f62bfc0b82f7b1640ea47e21a3c2eb53fb7b
Thami Bouchnafa <thami.bouchnafa@1und1.de>
2015-12-22 14:19:40 +0100

    module.exports server

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ var pjson = require('./package.json');
 var Webserver = require('./webserver.js');
 var server = new Webserver('./resources/websites', pjson.config.port);
 
-server.start(function () {
-    var websites = server.getAvailableWebsites();
-    console.log(JSON.stringify(websites, null, 2));
-});
+module.exports = server;
