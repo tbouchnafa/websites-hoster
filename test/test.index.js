036521c8aab557bf38f37e260a7c531fcaaba6a9
Thami Bouchnafa <thami.bouchnafa@1und1.de>
2016-04-05 16:39:27 +0200

    added test for starting server maximal 10 times

--- test/test.index.js
+++ test/test.index.js
@@ -2,51 +2,62 @@
 
 var assert = require('assert');
 var needle = require('needle');
-var pjson = require('../package.json');
+var async = require('async');
 
 describe('Webserver Class', function () {
 
-    var websites;
     var Webserver = require('../index');
     var server = new Webserver();
-
-    before(function (done) {
-        server.start(function (error, result) {
-            assert.ifError(error);
-            websites = result;
-            done();
-        });
-    });
-
-    after(function (done) {
-        server.close(done);
-    });
 
     describe('#start()', function () {
 
         it('check if websites were found', function (done) {
             this.timeout(60000);
-
-            assert.equal(Object.keys(websites).length > 0, true, 'no websites found');
-
-            assert.ok(websites['cassing.de']);
-            assert.equal(websites['cassing.de'].indexOf('/cassing.de/index.html') > -1, true);
-
-            needle.get(websites['cassing.de'], function (error, response) {
+            var websites;
+            server.start(function(error, websites) {
                 assert.ifError(error);
-                assert.ok(response.body);
-                done();
-            })
-        });
+                assert.equal(Object.keys(websites).length > 0, true, 'no websites found');
 
-        it('check start on the same server', function (done) {
-            this.timeout(60000);
-            server.start(function (error, result) {
-                assert.ifError(error);
-                assert.ok(result);
-                assert.equal(server.port > server.source_port, true);
-                done();
+                assert.ok(websites['cassing.de']);
+                assert.equal(websites['cassing.de'].indexOf('/cassing.de/index.html') > -1, true);
+
+                needle.get(websites['cassing.de'], function (error, response) {
+                    assert.ifError(error);
+                    assert.ok(response.body);
+                    server.close(done);
+                })
             });
+        });
+    });
+
+    describe('#start() maximal 10 times', function () {
+
+        it('check start', function (done) {
+            this.timeout(60000);
+
+            var start = function(cb) {
+                server.start(function(error, websites) {
+                    console.log(JSON.stringify(websites, null, 2));
+                    cb(error);
+                });
+            };
+
+            var close = function(cb) {
+                server.close(cb);
+            };
+
+            async.waterfall([start, start, start, start, start, start, start, start, start, start, start], function(error){
+                // could not start ther server 11 times error != null
+                assert.ok(error);
+                async.waterfall([close, close, close, close, close, close, close, close, close, close], function(error){
+
+                    // closing...
+                    assert.ifError(error);
+                    done();
+
+                });
+            });
+
         });
     });
 });
